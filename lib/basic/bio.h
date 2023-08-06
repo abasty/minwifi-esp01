6145b990b29f82692b91d84e371aa3cf944797e5
Alain Basty <alain.basty@free.fr>
2023-08-06 21:23:28 +0200

    WIP: Added new var structure

--- lib/basic/bio.h
+++ lib/basic/bio.h
@@ -90,6 +90,30 @@ typedef struct {
     };
 } var_t;
 
+// total size in bytes of a var
+// 1 float: sizeof(new_var_t) + sizeof(float) + (len(name) +  Len(name) % 4)
+// 1 string: sizeof(new_var_t) + (len(string) + 1) + (len(name) +  Len(name) % 4)
+// # floats: sizeof(new_var_t) + dim_count * 4 + P(dims) * 4 + (len(name) +  Len(name) % 4)
+//     A(n = P(i0, i1, ...)): numbers[n + #dims]
+// # strings: sizeof(new_var_t) + dim_count * 4 + P(dims) * 1 + (len(name) +  Len(name) % 4)
+//     A$(n = P(i0, i1, ...)): bytes[n + #dims]
+
+// P() = ((i0 - 1) * dims[1] + (i1 - 1)) * dims[2] + ...
+
+
+
+typedef struct {
+    uint8_t token;
+    uint8_t dim_count;    // 0 for simple vars
+    uint16_t name_ofs;    // Offset of the var name in var.bytes array
+    union {
+        uint32_t dims[0]; // size of each dimension. Do not exists in simple vars
+        float numbers[0]; // 1st element at numbers[dim_count], sizeof(float) == sizeof(uint32_t)
+        uint8_t bytes[0]; // 1st element at bytes[dim_count * size_of(uint32_t)]
+    };
+    // names: bytes[]
+} bvar_t;
+
 void bastos_init(bastos_io_t *_io);
 
 size_t bastos_send_keys(const char *keys, size_t n);
