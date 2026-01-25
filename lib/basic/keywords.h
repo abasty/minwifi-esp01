083e67228a201b327ff13d1cfc752c72d5c2458f
Alain BASTY <alain.basty@free.fr>
2026-01-25 19:31:21 +0100

    Implemented FILE and added example

--- lib/basic/keywords.h
+++ lib/basic/keywords.h
@@ -103,3 +103,4 @@
 #define TOKEN_KEYWORD_ORIGIN ((uint8_t) (101 | 0b10000000))
 #define TOKEN_KEYWORD_VKEY ((uint8_t) (102 | 0b10000000))
 #define TOKEN_KEYWORD_INDEX ((uint8_t) (103 | 0b10000000))
+#define TOKEN_KEYWORD_FILE ((uint8_t) (104 | 0b10000000))
