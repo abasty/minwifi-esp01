43ba6c1ba9e788f0feede667096e8091a0e03910
Alain BASTY <alain.basty@free.fr>
2026-06-14 16:34:54 +0200

    feat: remove obsolete scripts

--- galaxy-gw.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec ncat -kl -m1 -vvv 127.0.0.1 1963 -e "/usr/local/bin/websocat -b ws://galaxy.microtel.fr:50124"
--- hacker-gw.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec ncat -kl -m1 -vvv 127.0.0.1 1963 -e "/usr/local/bin/websocat -b ws://mntl.joher.com:2018/?echo"
